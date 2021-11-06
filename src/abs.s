c3e4f1f07f421d145c096770379bfc322c1d4b6e
Xinwei Xu <xinwei.xu@arm.com>
2021-11-06 16:43:27 +0100

    temp

--- src/abs.s
+++ src/abs.s
@@ -9,11 +9,9 @@
 #	a0 (int) the absolute value of the input
 # =================================================================
 abs:
-    # Prologue
 
-    # return 0
-    mv a0, zero
+    bgtz a0, finish
+    sub a0, zero, a0
 
-    # Epilogue
-
+finish:
     ret
