a3fa97f55467c940444613814dec43765dcc9e62
Xinwei Xu <xinwei.xu@arm.com>
2021-11-07 16:50:02 +0100

    argmax, relu

--- src/argmax.s
+++ src/argmax.s
@@ -17,18 +17,49 @@
 argmax:
 
     # Prologue
+    addi sp, sp, -4
+    sw ra, 0(sp)
 
+
+
+li t2, 1 # a flag indicating the first loop
 
 loop_start:
+  bge zero, a1, loop_end 
+  addi a1, a1, -1
+  slli t0, a1, 2
+  add t0, t0, a0
+  lw t1, 0(t0) ## t1 = a0[idx]
+
+  bnez t2,  set_flag # only for first loop
+
+  # branch if t1 < t3 (prev elem)
+  blt t1, t3, loop_continue
+
+  mv t4, a1 # store the index of a larger element
+
+  mv t3, t1 # t3 prev
+
+  j loop_continue
+  
+
+set_flag:
+  mv t4, a1
+  mv t2, zero
+  mv t3, t1 # t3 prev
 
 
 loop_continue:
+  j loop_start
 
 
 loop_end:
     
+    mv a0, t4
 
     # Epilogue
+    sw ra, 0(sp)
+    addi sp, sp, 4
 
 
     ret
--- src/relu.s
+++ src/relu.s
@@ -14,25 +14,34 @@
 # ==============================================================================
 relu:
     # Prologue
+    addi sp, sp, -4
+    sw ra, 0(sp)
+
 
 
 loop_start:
-    
 
+  bge zero, a1, loop_end
+  addi a1, a1, -1
 
+  slli t0, a1, 2 # t0 = index *4               multiple the index by 4 to get the address offset
+  add t1, a0, t0 # t1 = start_addr + index*4   addr of elem   calc the addr of the src element
+  lw t2, 0(t1)      # load the element to register
 
+  bge t2, zero, loop_continue # if elem >= 0, then does nothing but continue with the loop
 
-
+  sw zero, 0(t1)
 
 
 loop_continue:
-
+  j loop_start
 
 
 loop_end:
 
-
     # Epilogue
+    lw ra, 0(sp)
+    addi, sp, sp, 4
 
     
 	ret
